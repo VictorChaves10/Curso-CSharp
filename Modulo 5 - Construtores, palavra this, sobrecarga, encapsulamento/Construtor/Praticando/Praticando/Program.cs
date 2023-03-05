9e3c40800eb4a33f0007d9414447148e731b8d13
Victor Chaves <victor.chaves460@gmail.com>
2023-03-05 08:08:22 -0300

    .

--- Modulo 5 - Construtores, palavra this, sobrecarga, encapsulamento/Construtor/Praticando/Praticando/Program.cs
+++ Modulo 5 - Construtores, palavra this, sobrecarga, encapsulamento/Construtor/Praticando/Praticando/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.WriteLine("Entre os dados do produto:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("\nDados do produto: " + p);
+ 
+            Console.Write("\nDigite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
