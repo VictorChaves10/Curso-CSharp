0c0a9fcad3b17ede96ba5b35ab9d318cbc7100b2
Victor Chaves <victor.chaves460@gmail.com>
2023-05-06 17:24:08 -0300

    Desafio capitulo 5

---
+++ Modulo 5 - Construtores, palavra this, sobrecarga, encapsulamento/Desafio/ContaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio
+{
+    internal class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double SaldoConta { get; private set; }
+
+
+        public ContaBancaria(int numConta, string nome)
+        {
+            NumeroConta = numConta;
+            NomeTitular = nome;
+        }
+        public ContaBancaria(int numConta, string nome, double deposito) : this(numConta, nome)
+        {
+            SaldoConta = deposito;
+        }
+
+        public void Deposito(double valor)
+        {
+            SaldoConta += valor;
+        }
+
+        public  void Saque(double valor)
+        {
+            SaldoConta -= valor + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {NomeTitular}, Saldo $ {SaldoConta.ToString("F2")}"; 
+        }
+
+
+    }
+}
---
+++ Modulo 5 - Construtores, palavra this, sobrecarga, encapsulamento/Desafio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System;
+using Desafio;
+
+Console.Write("Entre o número da conta: ");
+int numConta = int.Parse(Console.ReadLine());
+
+Console.Write("Entre o titular da conta: ");
+string nome = Console.ReadLine();
+
+Console.Write("Haverá depósito inicial (s/n)? ");
+char depositoInicial = Char.Parse(Console.ReadLine().ToUpper());
+
+ContaBancaria user1 = new ContaBancaria(numConta, nome);
+
+while(depositoInicial != 'S' && depositoInicial != 'N')
+{
+    Console.Write("Favor informar uma opção válida : ");
+    depositoInicial = Char.Parse(Console.ReadLine().ToUpper());
+}
+
+
+if(depositoInicial == 'S')
+{
+    Console.Write("Entre o valor de depósito inicial: ");
+    user1.Deposito(double.Parse(Console.ReadLine()));
+}
+
+Console.WriteLine("\n\nDados da conta:");
+Console.WriteLine(user1);
+
+Console.Write("\n\nEntre um valor para depósito: ");
+user1.Deposito(double.Parse(Console.ReadLine()));
+
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(user1);
+
+Console.Write("\n\nEntre um valor para saque: ");
+user1.Saque(double.Parse(Console.ReadLine()));
+
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(user1);
