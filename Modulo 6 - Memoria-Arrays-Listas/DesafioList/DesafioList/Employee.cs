df09d2feae7cc9450d2a2a1c4d94cc896863d71d
Victor Chaves <victor.chaves460@gmail.com>
2023-05-18 21:10:04 -0300

    Create DesafioList

---
+++ Modulo 6 - Memoria-Arrays-Listas/DesafioList/DesafioList/Employee.cs
@@ -0,0 +1,26 @@
+﻿internal class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public double Salary { get; set; }
+
+    public Employee(int id, string name, double salary)
+    {
+        Id = id;
+        Name = name;
+        Salary = salary;
+    }
+
+    public void IncreaseSlary(double porcentage)
+    {
+        Salary += (porcentage * Salary) / 100.00;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}, {Name}, {Salary.ToString("F2")}";
+    }
+}
+
+
---
+++ Modulo 6 - Memoria-Arrays-Listas/DesafioList/DesafioList/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections.Generic;
+
+
+List<Employee> employees = new List<Employee>();
+Console.Write("How many employees will be registered? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"\nEmploye #{i + 1}:");
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salary: ");
+    double salary = double.Parse(Console.ReadLine());
+
+    employees.Add(new Employee(id, name, salary));
+
+}
+
+Console.Write("\n\nEnter the employee id that will have salary increase: ");
+int findId = int.Parse(Console.ReadLine());
+
+
+Employee result = employees.Find(x => x.Id == findId);
+
+if (result != null)
+{
+    Console.Write("Enter the percentage: ");
+    double position = double.Parse(Console.ReadLine());
+    result.IncreaseSlary(position);
+
+}
+else
+{
+    Console.WriteLine("This id does not exist!");
+}
+
+Console.WriteLine("\nUpdate list of employees:");
+
+foreach (Employee obj in employees)
+{
+    Console.WriteLine(obj);
+}
