5673d71ca4f3043134e12fb823d30d8b052aa2d6
Victor Chaves <victor.chaves460@gmail.com>
2023-06-03 15:57:20 -0300

    Create ex Substring

---
+++ Modulo 8 - Enumeracoes-Composicao/ExStringBuilder/ExStringBuilder/Entities/Post.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExStringBuilder.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Coment1> Comments { get; set; } = new List<Coment1>();
+        
+        public Post() { }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Coment1 coment) { Comments.Add(coment); }
+
+        public void RemoveComment(Coment1 coment) {  Comments.Remove(coment); }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendLine(Title);
+            sb.AppendLine($"{Likes} Likes - {Moment.ToString("dd/MM/yyyy HH:mm:ss")}");
+            sb.AppendLine(Content);
+
+            foreach(Coment1 coment in Comments)
+            {
+                sb.AppendLine(coment.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Modulo 8 - Enumeracoes-Composicao/ExStringBuilder/ExStringBuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using ExStringBuilder.Entities;
+
+Coment1 c1 = new Coment1("Have a nice trip!");
+Coment1 c2 = new Coment1("Wow that's awesome!");
+
+Post p1 = new Post (
+
+    DateTime.Now,
+    "Traveling to New Zealand",
+    "I'm going to visit this wonderful country!",
+    12
+);
+
+p1.AddComment(c1);
+p1.AddComment(c2);
+
+Coment1 c3 = new Coment1("Good Night");
+Coment1 c4 = new Coment1("May the Force be with you");
+
+Post p2 = new Post(
+    DateTime.Now,
+    "Good Nitght guys",
+    "See you tomorrow",
+    5
+);
+
+p2.AddComment(c3);
+p2.AddComment(c4);
+
+Console.WriteLine(p1);
+Console.WriteLine();
+Console.WriteLine(p2);
