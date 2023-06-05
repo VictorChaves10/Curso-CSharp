c16f0696e15638b2d59e27ab79f17bf60c550e1f
Victor Chaves <victor.chaves460@gmail.com>
2023-06-05 18:29:08 -0300

    Update Heranca

--- Modulo 9 - Herança e polimorfismo/Heranca01/Heranca01/Entities/Account.cs
+++ Modulo 9 - Herança e polimorfismo/Heranca01/Heranca01/Entities/Account.cs
@@ -8,5 +8,27 @@ namespace Heranca01.Entities
 {
     internal class Account
     {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number,string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amnount)
+        {
+            Balance -= amnount;
+        }
+        public void Deposit(double ammount)
+        {
+            Balance += ammount;
+        }
+
     }
 }
--- Modulo 9 - Herança e polimorfismo/Heranca01/Heranca01/Entities/BusinessAccount.cs
+++ Modulo 9 - Herança e polimorfismo/Heranca01/Heranca01/Entities/BusinessAccount.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Heranca01.Entities
 {
-    internal class BusinessAccount
+    internal class BusinessAccount : Account
     {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() { }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit):base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;              
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
     }
+    }
+    
 }
