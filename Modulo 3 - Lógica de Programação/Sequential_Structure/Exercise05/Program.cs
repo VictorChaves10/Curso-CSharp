2947c954c098e0712b41fb87a93f5322824f191a
Victor Chaves <victor.chaves460@gmail.com>
2024-05-13 16:12:29 -0300

    refactor: Ajustando a soution do modulo 3

---
+++ Modulo 3 - Lógica de Programação/Sequential_Structure/Exercise05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+
+Console.Write("Digite o código da peça 1, a quantidade e o valor unitário: ");
+
+string[] peca1 = Console.ReadLine().Split(' ');
+
+int codig = int.Parse(peca1[0]);
+
+int quant = int.Parse(peca1[1]);
+
+double preco = double.Parse(peca1[2], CultureInfo.InvariantCulture);
+
+double sum = quant * preco;
+
+Console.Write("Digite o código da peça 2, a quantidade e o valor unitário: ");
+
+string[] peca2 = Console.ReadLine().Split(' ');
+
+codig = int.Parse(peca2[0]);
+
+quant = int.Parse(peca2[1]);
+
+preco = double.Parse(peca2[2], CultureInfo.InvariantCulture);
+
+sum += quant * preco;
+
+Console.WriteLine("VALOR A PAGAR: R$ " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
---
+++ Modulo 3 - Lógica de Programação/While_Structure/Exercise01/Program.cs
@@ -0,0 +1,81 @@
+﻿
+int tentativas = 0;
+string senha;
+
+Console.WriteLine("Digite uma senha com mínimo 6 dígitos: ");
+senha = Console.ReadLine();
+
+bool resultado = Senha(senha);
+
+while (resultado == false)
+{
+    Console.Clear();
+    Console.WriteLine("Senha inválida");
+    Console.WriteLine("Digite uma senha com o mínimo 6 dígitos: ");
+
+    senha = Console.ReadLine();
+    resultado = Senha(senha);
+}
+
+Console.WriteLine("Repita sua senha: ");
+string repeatSenha = Console.ReadLine();
+
+
+if (senha == repeatSenha)
+{
+    Console.WriteLine("Senha salva com sucesso!");
+
+}
+else
+{
+    while (repeatSenha != senha)
+    {
+        Console.WriteLine("As senhas não conferem!");
+        Console.WriteLine("Repita sua senha: ");
+        repeatSenha = Console.ReadLine();
+    }
+
+    Console.WriteLine("Senha salva com Sucesso!");
+
+}
+
+Console.Write("Para realizar o login, digite sua senha: ");
+string loginSenha = Console.ReadLine();
+
+while (loginSenha != senha && tentativas < 3)
+{
+    Console.WriteLine("Senha inválida");
+    tentativas++;
+    Console.Write("Digite novamente sua senha: ");
+    loginSenha = Console.ReadLine();
+
+    if (tentativas == 2)
+    {
+        Console.WriteLine("Atenção! Se errar novamente sua senha será bloqueada! ");
+
+    }
+
+}
+
+if (tentativas <= 3)
+{
+    Console.WriteLine("Login realizado com sucesso!"); 
+}
+else
+{
+    Console.WriteLine("Bloqueado");
+}
+
+static bool Senha(string a)
+{
+    bool verdadeiro;
+    if (a.Length >= 6)
+    {
+        verdadeiro = true;
+    }
+    else
+    {
+        verdadeiro = false;
+    }
+    return verdadeiro;
+}
